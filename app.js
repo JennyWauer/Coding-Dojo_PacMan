09a0a46c9327df6e8e25f5bda9bcb17decbb1c31
JennyWauer <64482030+JennyWauer@users.noreply.github.com>
2020-11-11 14:41:45 -0600

    Created displayWorld function

--- app.js
+++ app.js
@@ -8,3 +8,14 @@ var world =[
     [2,1,1,1,1,1,1,1,1,2],
     [2,2,2,2,2,2,2,2,2,2],
 ];
+
+function displayWorld(){
+    var output = '';
+
+    for(var i=0; i<world.length; i++){
+        for(var j=0; j<world.length; j++){
+            output = output . world[i][j];
+        }
+    }
+    console.log(output)
+}
